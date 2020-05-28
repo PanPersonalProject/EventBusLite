d3b6a73b5bfefdfccca4ea1fedaa2b686d4995d2
panqi <panqi@tiange.com>
2020-05-28 18:21:38 +0800

    EventBusLite Test代码

--- app/src/main/java/pan/lib/eventbuslite/MainActivity.kt
+++ app/src/main/java/pan/lib/eventbuslite/MainActivity.kt
@@ -2,6 +2,8 @@ package pan.lib.eventbuslite
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import pan.lib.eventbus.EventBusLite
@@ -17,15 +19,30 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             EventBusLite.getInstance().post(TestEvent())
         }
+        button2.setOnClickListener {
+            EventBusLite.getInstance().post(TestEvent2())
+        }
         bt_secondActivity.setOnClickListener {
             startActivity(Intent(this, SecondActivity::class.java))
         }
     }
 
 
-    @Subscribe(threadMode = ThreadMode.POSTING)
+    @Subscribe(threadMode = ThreadMode.MAIN)
     fun testMethod(event: TestEvent) {
         tv_content.text = event.str
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun test2(event: TestEvent) {
+        Toast.makeText(this, event.str, Toast.LENGTH_SHORT).show();
+    }
+
+    @Subscribe(threadMode = ThreadMode.ASYNC)
+    fun event2(event: TestEvent2) {
+        Looper.prepare()
+        Toast.makeText(this, """子线程 ${Thread.currentThread().name}""", Toast.LENGTH_SHORT).show();
+        Looper.loop()
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/pan/lib/eventbuslite/TestEvent2.kt
@@ -0,0 +1,9 @@
+package pan.lib.eventbuslite
+
+/**
+ * Author:         pan qi
+ * CreateDate:     2020/5/27 16:03
+ */
+class TestEvent2 {
+    var str = "TestEvent2"
+}
